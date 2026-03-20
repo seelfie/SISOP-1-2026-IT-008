2457b4f596f0175a6e03909a4af7e298b17d3ed8
Silfi R <5027251008@student.its.ac.id>
2026-03-20 14:10:14 +0700

    ganti dikit

--- soal_2/ekspedisi/peta-gunung-kawi/nemupusaka.sh
+++ soal_2/ekspedisi/peta-gunung-kawi/nemupusaka.sh
@@ -2,7 +2,7 @@
 
 #--mencari titik pusat--
 awk -F',' 'NR == 1 {lat1=$3; lon1=$4}
-	   NR == 3 {lat2=$3; lon2=$4}
+	       NR == 3 {lat2=$3; lon2=$4}
 END {
       longitude_pusat = (lon1 + lon2) / 2
       latitude_pusat = (lat1 + lat2) / 2 
