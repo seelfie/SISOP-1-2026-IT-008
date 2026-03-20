17d9f1e1bb106c781687aa29bd0200f81694b810
seelfie <5027251008@student.its.ac.id>
2026-03-20 14:01:31 +0700

    ntar dilanjut

--- soal_1/KANJ.sh
+++ soal_1/KANJ.sh
@@ -4,32 +4,32 @@ BEGIN {
 	FS=","
 	pilihan = ARGV[2]
 	delete ARGV[2]
-}
-#--a--
-NR>1 && pilihan == "a" {count++}
+      }
+      #--a--
+      NR>1 && pilihan == "a" {count++}
 
-#--b--
-NR>1 && pilihan == "b" {gsub(/\r/, "", $4); gerbong[$4]++}
+      #--b--
+      NR>1 && pilihan == "b" {gsub(/\r/, "", $4); gerbong[$4]++}
 
-#--c--
-NR>1 && pilihan == "c" {if ($2 > max) {max = $2; nama = $1}}
+      #--c--
+      NR>1 && pilihan == "c" {if ($2 > max) {max = $2; nama = $1}}
 
-#--d--
-NR>1 && pilihan == "d" {sum+=$2; count++}
+      #--d--
+      NR>1 && pilihan == "d" {sum+=$2; count++}
 
-#--e--
-NR>1 && pilihan == "e" && $3 == "Business" {kelas++}
+      #--e--
+      NR>1 && pilihan == "e" && $3 == "Business" {kelas++}
 
-END {
-      if (pilihan == "a") print "Jumlah seluruh penumpang KANJ adalah", count, "orang"
+      END {
+            if (pilihan == "a") print "Jumlah seluruh penumpang KANJ adalah", count, "orang"
 
-      else if (pilihan == "b") print "Jumlah gerbong penumpang KANJ adalah", length(gerbong)
+            else if (pilihan == "b") print "Jumlah gerbong penumpang KANJ adalah", length(gerbong)
 
-      else if (pilihan == "c") print nama, "adalah penumpang kereta tertua dengan usia", max, "tahun"
+            else if (pilihan == "c") print nama, "adalah penumpang kereta tertua dengan usia", max, "tahun"
 
-      else if (pilihan == "d") printf "Rata-rata usia penumpang adalah %.0f", sum/count, "tahun"
+            else if (pilihan == "d") printf "Rata-rata usia penumpang adalah %.0f", sum/count, "tahun"
 
-      else if (pilihan == "e") print "Jumlah penumpang business class ada", kelas, "orang"
+            else if (pilihan == "e") print "Jumlah penumpang business class ada", kelas, "orang"
 
-      else print  "Soal tidak dikenali. Gunakan a, b, c, d, atau e.\n" "Contoh penggunaan: awk -f KANJ.sh data.csv (a/b/c/d/e)"
+            else print  "Soal tidak dikenali. Gunakan a, b, c, d, atau e.\n" "Contoh penggunaan: awk -f KANJ.sh data.csv (a/b/c/d/e)"
 } 
